fe3bc20bcd0a4e005f698e505c1b026073679634
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2021-04-25 01:41:58 +0600

    wish list added

--- src/App.js
+++ src/App.js
@@ -5,20 +5,73 @@ import Country from './components/Country/Country';
 
 function App() {
     const [countries, setCountries] = useState([]);
+    const [wishlist, setWishlist] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.eu/rest/v2/all')
             .then((res) => res.json())
             .then((countries) => setCountries(countries));
-    });
+        console.log('useEffect');
+    }, '');
+
+    const handleWishList = (country) => {
+        setWishlist([...wishlist, country]);
+        console.log(country.flag);
+    };
+
+    // Search Country
+
+    const searchCountry = (searchTerm) => {
+        console.log(searchTerm);
+        const filteredCountries = countries.filter((country) => {
+            return searchTerm.toLowerCase() == country.name.toLowerCase()
+                ? true
+                : false;
+        });
+        console.log(filteredCountries);
+
+        setCountries(filteredCountries);
+    };
+
+    // Search Country
 
     return (
         <div className='App'>
             <h2>Rest countries</h2>
+            <h4>
+                Total Countries List :
+                {countries.length === undefined ? 0 : countries.length}
+            </h4>
+            {/* <h3>Wish list Courtries: {singleCountry.flag}</h3> */}
+            <div>
+                Wish list Courtries:
+                {wishlist.map((couFlag) => (
+                    <img src={couFlag.flag} height='25px' width='50px' />
+                ))}
+            </div>
+
+            {/* // Search Country */}
+
+            <div className='form'>
+                <input
+                    onKeyPress={function (e) {
+                        searchCountry(e.target.value);
+                    }}
+                    type='search'
+                    name=''
+                    id=''
+                    placeholder='Search Country'
+                />
+            </div>
+
+            {/* // Search Country */}
+
             <p>
-                Countries List:
                 {countries.map((country) => (
-                    <Country country={country} />
+                    <Country
+                        handleWishList={handleWishList}
+                        country={country}
+                    />
                 ))}
             </p>
         </div>
@@ -42,3 +95,17 @@ export default App;
 //   fetchMyAPI();
 //   return () => { didCancel = true; }; // Remember if we start fetching something else
 // }, [productId]);
+
+// useEffect(() => {
+//   let didCancel = false;
+//   async function fetchMyAPI() {
+//     let url = 'https://restcountries.eu/rest/v2/all';
+//     let config = {};
+//     const response = await fetch(url);
+//     if(!didCancel) {
+//       console.log(response)
+//     }
+//   }
+//   fetchMyAPI();
+//   return ()=> {didCancel = true; }
+// })
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './Country.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
 const Country = (props) => {
     const { name, capital, cioc, flag, population, region } = props.country;
 
     return (
-        <div className='courtries-container'>
-            <div>
-                <img src={flag} alt='' width='200px' height='100px' />
-                <h1>{name}</h1>
-                <h2>{capital}</h2>
-                <h3>{cioc === '' ? 'Not Found' : cioc}</h3>
-                <p>{population}</p>
-                <p>{region}</p>
+        <div className='flex-container'>
+            <div className='courtries-container'>
+                <div className='single-item'>
+                    <img src={flag} alt='' width='200px' height='100px' />
+                    <h1>{name}</h1>
+                    <h2>{capital}</h2>
+                    <h3>{cioc === '' ? 'Not Found' : cioc}</h3>
+                    <p>{population}</p>
+                    <p>{region}</p>
+                    <button
+                        className='btn btn-danger btn-lg'
+                        onClick={() => props.handleWishList(props.country)}>
+                        Add to
+                        <FontAwesomeIcon icon={faHeart} /> list
+                    </button>
+                </div>
             </div>
         </div>
     );
